f6f254eb6f754dbebe214d82c571ac9c3fa27e64
Sam He <sam.he@cargosmart.com>
2017-12-05 18:48:42 +0800

    debug the scan

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,7 +20,6 @@ function allTest() {
 }
 
 function test() {
-  // let { app, db } = getServer().getInstance();
   return gulp.src(testFiles)
     .pipe(mocha({
       reporter: 'spec',
@@ -45,11 +44,14 @@ function coverage(done) {
 }
 
 function codeScan(done) {
+  console.log(`env to scan is ${process.env.SONAR_TOKEN}`);
   sonarqubeScanner({
     serverUrl : 'https://sonarcloud.io',
     token : process.env.SONAR_TOKEN,
     options : {
       'sonar.organization': 'samhe-github',
+      'sonar.projectKey': ' tdd-examples-nodejs',
+      'sonar.projectName': ' tdd-examples-nodejs',
       'sonar.sources': '.',
       'sonar.inclusions': 'modules/*/**/*.js',
       'sonar.exclusions': 'modules/*/test/**/*',
